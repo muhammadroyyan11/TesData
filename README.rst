71db21a1b1b657759275d378b8a3e0521b8ce52a
muhammadroyyan11 <royyanmz87@gmail.com>
2022-06-30 14:34:26 +0700

    start

---
+++ README.rst
@@ -0,0 +1,22 @@
+###################
+Build module codeigniter 3 Nyoba
+###################
+
+**************************
+Dududu
+**************************
+- Buat database
+- configure db dulu sesuaikan sama nama yang telah dibuat
+
+**************************
+Login default super admin
+**************************
+- admin@admin.com
+- password
+
+
+**************************
+Soon
+**************************
+- auto relation table generator
+- generate REST API Table
---
+++ application/modules/m_crud_generator/models/M_crud_generator_model.php
@@ -0,0 +1,71 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class M_crud_generator_model extends MY_Model{
+
+  private $table        = "modules_crud_generator";
+  private $primary_key  = "id";
+  private $column_order = array('module', 'module_name', 'table', 'created_at');
+  private $order        = array('modules_crud_generator.id'=>"DESC");
+  private $select       = "modules_crud_generator.id,modules_crud_generator.module,modules_crud_generator.module_name,modules_crud_generator.table,modules_crud_generator.created_at";
+
+public function __construct()
+	{
+		$config = array(
+      'table' 	      => $this->table,
+			'primary_key' 	=> $this->primary_key,
+		 	'select' 	      => $this->select,
+      'column_order' 	=> $this->column_order,
+      'order' 	      => $this->order,
+		 );
+
+		parent::__construct($config);
+	}
+
+  private function _get_datatables_query()
+    {
+      $this->db->select($this->select);
+      $this->db->from($this->table);
+
+      if(isset($_POST['order'])) // here order processing
+       {
+           $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+       }
+       else if(isset($this->order))
+       {
+           $order = $this->order;
+           $this->db->order_by(key($order), $order[key($order)]);
+       }
+
+    }
+
+
+    public function get_datatables()
+    {
+        $this->_get_datatables_query();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function count_filtered()
+    {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function count_all()
+    {
+      $this->db->select($this->select);
+      $this->db->from("$this->table");
+      return $this->db->count_all_results();
+    }
+
+
+
+}
+
+/* End of file M_crud_generator_model.php */
+/* Location: ./application/modules_crud_generator/m_crud_generator/models/M_crud_generator_model.php */
